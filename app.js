93bcda557a4793a73744ec7d65b1821706aca789
Trần Tấn Dũng <85373307+tandung10x@users.noreply.github.com>
2022-08-24 20:57:14 +0700

    update

--- app.js
+++ app.js
@@ -5,20 +5,47 @@ function updateTodoList() {
     eleTodoList.innerHTML = '';
 
     for(var i=0; i<todos.length; i++) {
-        eleTodoList.innerHTML +='<li onClick="onTodoClick(' + i +')">'+todos[i]+'</li>';
+        eleTodoList.innerHTML += `
+            <li>` + 
+                todos[i] +
+                `<span>
+                    <i class="fas fa-check" id="check`+i+`" onClick= "onCheckClick(`+i+`)"></i>
+                    <i class="fas fa-trash" onClick="onDeleteClick(`+i+`)"></i>
+                </span>
+            </li>`;
     }
+    
+    eleTodoList.innerHTML +='<button class="btn-clear" onClick="onClear()">Clear All</button>'
 }
 
 function onAddClick() {
     var newTodo = document.getElementById('newTodo').value;
-    todos.push(newTodo);
+    if(newTodo.trim()!='') todos.push(newTodo);
 
     updateTodoList();
 
     document.getElementById('newTodo').value = '';
 }
 
-function onTodoClick(index) {
+function onDeleteClick(index) {
     todos.splice(index, 1);
     updateTodoList();
+}
+
+function onClear() {
+    todos = [];
+    updateTodoList();
+}
+
+let cnt = new Array(50).fill(0);
+function onCheckClick(index) {
+    let c = '#check' + index;
+    if(cnt[index]%2==0) {
+        $(c).css('color', 'green');
+        cnt[index]++;
+    }
+    else {
+        $(c).css('color', 'white');
+        cnt[index]++;
+    }
 }
